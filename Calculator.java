a59bc12537250b5ea1379f4b08bd38d52414fa0c
Jawad <f219478@cfd.nu.edu.pk>
2023-10-06 11:08:17 +0500

    Added Divide func in Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -8,5 +8,22 @@ public class Calculator {
 	public double subtract(double n1, double n2) {
 		return n1 - n2;
 	}
+	
+	public double divide(int n1, int n2) {
+		
+		double result = 0;
+		
+		try {
+			result =  n1 / n2;
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		return result;
+	}
+	
+	
+	
+	
 
 }
